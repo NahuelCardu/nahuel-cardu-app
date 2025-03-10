5e187df6b3533822866d847fa235def9ee1e8b2e
NahuelCardu <109104502+NahuelCardu@users.noreply.github.com>
2025-03-10 11:40:14 -0300

    CKP correcciones de modulo de seguridad y react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Login } from './logger';
-import Working from './working'; // Importamos el componente de "En construcción"
+import Working from './working';
+import Client from './Client';
 import './App.css';
 
 function App() {
-  const navigate = useNavigate(); // Hook para redirigir dentro de la SPA
-  const location = useLocation(); // Hook para obtener la ubicación actual
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
 
-  // Efecto para cambiar la clase en el body dependiendo de la ruta activa
+  // Efecto para manejar estilos dependiendo de la ruta
   useEffect(() => {
     if (location.pathname === '/working') {
       document.body.classList.add('working-page');
@@ -17,27 +20,38 @@ function App() {
     }
   }, [location]);
 
+  // Manejar la redirección segura a /client
+  const handleClientClick = async () => {
+    if (!isAuthenticated) {
+      await loginWithRedirect();
+    } else {
+      navigate('/client');
+    }
+  };
+
   return (
     <div className="home-container">
-      <div className="image-container" onClick={() => navigate('/login')}>
-        <img src="/img/Client.png" alt="Clientes" className="image" style={{ padding: '3rem' }} />
-        <p>Clientes</p>
-      </div>
+      {location.pathname !== '/client' && (
+        <>
+          <div className="image-container" onClick={handleClientClick}>
+            <img src="/img/Client.png" alt="Clientes" className="image" style={{ padding: '3rem' }} />
+            <p>Clientes</p>
+          </div>
 
-      <div id='' className="image-container" onClick={() => window.open('/working', '_blank')}>
-        <img src="/img/Restaurant.png" alt="Restaurantes" className="image" />
-        <p>Restaurantes</p>
-      </div>
-
-      {/* Definimos las rutas */}
+          <div className="image-container" onClick={() => window.open('/working', '_blank')}>
+            <img src="/img/Restaurant.png" alt="Restaurantes" className="image" />
+            <p>Restaurantes</p>
+          </div>
+        </>
+      )}
+      
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/working" element={<Working />} />
+        <Route path="/client" element={<Client />} />
       </Routes>
     </div>
   );
 }
-
-
 
 export default App;
